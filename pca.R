6ad3b6d058615c857acf22ba730e28c81c0840ab
Phillp Månsson <34268692+wee-analyze@users.noreply.github.com>
2018-02-06 11:41:06 +0100

    Update pca.R

--- pca.R
+++ pca.R
@@ -12,14 +12,13 @@ n <- ncol(scaled_data) #number of features
 # Calculate covariance matrix
 covariance_matrix <- (1/m) * (t(scaled_data) %*% scaled_data)
 
-# Calculate Singular Value Decomposition of Matrix to get eigenvectors
-# and singular values. 
+# Calculate Singular Value Decomposition of Matrix to get eigenvectors and singular values. 
 svd_return <- svd(covariance_matrix)
 principal_components <- svd_return$u
 singular_values <- svd_return$d
 
 # Determine number of principal components (k) which will give you at least 95% of variance retained
-k <- 9 #number_here that gives >= 95% variance
+k <- 9 #choose number of principal components here that gives >= 95% variance
 reduced_pc <- principal_components[,1:k]
 reduced_dataset <- scaled_data %*% reduced_pc
 
